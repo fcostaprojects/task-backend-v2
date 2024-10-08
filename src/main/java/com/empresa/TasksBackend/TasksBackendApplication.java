885e856fddeee3727b153fad96776c9f98e94460
fcostaprojects <flaviocostarj@gmail.com>
2024-10-08 14:27:23 -0300

    alterando a classe de inicialização

--- src/main/java/com/empresa/TasksBackend/TasksBackendApplication.java
+++ src/main/java/com/empresa/TasksBackend/TasksBackendApplication.java
@@ -2,12 +2,20 @@ package com.empresa.TasksBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TasksBackendApplication {
+public class TasksBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TasksBackendApplication.class);
 	}
 
 }
