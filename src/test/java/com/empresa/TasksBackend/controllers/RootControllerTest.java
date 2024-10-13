ff861a281c43e052378860bf39c45d814855615e
fcostaprojects <flaviocostarj@gmail.com>
2024-10-13 20:09:02 -0300

    testes ajustados

---
+++ src/test/java/com/empresa/TasksBackend/controllers/RootControllerTest.java
@@ -0,0 +1,22 @@
+package com.empresa.TasksBackend.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest(RootController.class)
+public class RootControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveRetorarHelloWord() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(content().string(is("Hello World!")));
+    }
+}
