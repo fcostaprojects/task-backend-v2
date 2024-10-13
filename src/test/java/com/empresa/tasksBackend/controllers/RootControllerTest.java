2affd6905328c8ad4cea7284cc5d32ab1b3c3a9c
fcostaprojects <flaviocostarj@gmail.com>
2024-10-13 20:41:21 -0300

    ajuste nos testes

--- src/test/java/com/empresa/tasksBackend/controllers/RootControllerTest.java
+++ src/test/java/com/empresa/tasksBackend/controllers/RootControllerTest.java
@@ -1,23 +1,26 @@
 package com.empresa.tasksBackend.controllers;
 
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@WebMvcTest(RootController.class)
 public class RootControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
     public void deveRetorarHelloWord() throws Exception {
-        ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
-        Assertions.assertThat(response.getStatusCodeValue()).isEqualTo(200);
-        Assertions.assertThat(response.getBody()).isEqualTo("Hello World!");
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
     }
 }
