c79f066915de149f561872690aee55f5a39bc1c0
fcostaprojects <flaviocostarj@gmail.com>
2024-10-14 19:25:54 -0300

    ajuste de testes

--- src/main/java/com/empresa/tasksBackend/controllers/RootController.java
+++ src/main/java/com/empresa/tasksBackend/controllers/RootController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value ="/")
 public class RootController {
 
-    @GetMapping("/")
+    @GetMapping
     public String hello() {
         return "Hello World!";
     }
