80b20177369b486678d252b413adb7568f3bb051
fcostaprojects <flaviocostarj@gmail.com>
2024-10-14 21:05:33 -0300

    ajuste de controlador

--- src/main/java/com/empresa/tasksBackend/controllers/RootController.java
+++ src/main/java/com/empresa/tasksBackend/controllers/RootController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value ="/")
 public class RootController {
 
-    @GetMapping
+    @GetMapping("/")
     public String hello() {
         return "Hello World!";
     }
